51657b3245fb8406d98e9cf4297931782e5c99f9
RubyTrap2 <rubymedlock.dev@gmail.com>
2025-02-26 00:39:12 -0700

    Add files via upload

---
+++ Images/script.js
@@ -0,0 +1,143 @@
+window.onload = function(){
+    const screenDiv = this.document.getElementById('screenDiv');
+
+
+    // this is probably for loopable but i couldnt be bothered for just a linktree
+
+    // discord
+    const discord = this.document.getElementById('discord');
+    //click
+    discord.addEventListener('click', function(){
+        var elem = document.createElement('p');
+        elem.textContent = "Copied to clipboard:\nruby_programming";
+        elem.id = 'screenText';
+        navigator.clipboard.writeText('ruby_programming');
+        screenDiv.innerHTML = ''
+        screenDiv.style.backgroundImage = 'none'
+        screenDiv.appendChild(elem);
+    });
+
+    //hover
+    var isInside = false;
+    discord.addEventListener('mouseover', function(){
+        if (!isInside) {
+            isInside = true;
+            screenDiv.style.backgroundImage = "url('./Images/Discord Background.png')";
+        }
+    });
+    discord.addEventListener('mouseout', () => {
+        screenDiv.innerHTML = ''
+        screenDiv.style.backgroundImage = 'none'
+        isInside = false;
+      });
+
+
+
+    //Youtube
+    const youtube = this.document.getElementById('youtube');
+
+    //hover
+    youtube.addEventListener('mouseover', function(){
+        if (!isInside) {
+            isInside = true;
+            screenDiv.style.backgroundImage = "url('./Images/Youtube Background.png')";
+        }
+    });
+    youtube.addEventListener('mouseout', () => {
+        screenDiv.innerHTML = ''
+        screenDiv.style.backgroundImage = 'none'
+        isInside = false;
+      });
+
+
+
+    //Twitch
+    const twitch = this.document.getElementById('twitch');
+
+    //hover
+    twitch.addEventListener('mouseover', function(){
+        if (!isInside) {
+            isInside = true;
+            screenDiv.style.backgroundImage = "url('./Images/Twitch Background.png')";
+        }
+    });
+    twitch.addEventListener('mouseout', () => {
+        screenDiv.innerHTML = ''
+        screenDiv.style.backgroundImage = 'none'
+        isInside = false;
+      });
+
+
+
+    //Pronoun
+    const pronoun = this.document.getElementById('pronoun');
+
+    //hover
+    pronoun.addEventListener('mouseover', function(){
+        if (!isInside) {
+            isInside = true;
+            screenDiv.style.backgroundImage = "url('./Images/Pronoun Background.png')";
+        }
+    });
+    pronoun.addEventListener('mouseout', () => {
+        screenDiv.innerHTML = ''
+        screenDiv.style.backgroundImage = 'none'
+        isInside = false;
+      });
+
+
+
+    //Steam
+    const steam = this.document.getElementById('steam');
+
+    //hover
+    steam.addEventListener('mouseover', function(){
+        if (!isInside) {
+            isInside = true;
+            screenDiv.style.backgroundImage = "url('./Images/Steam Background.png')";
+        }
+    });
+    steam.addEventListener('mouseout', () => {
+        screenDiv.innerHTML = ''
+        screenDiv.style.backgroundImage = 'none'
+        isInside = false;
+      });
+
+
+
+    //Tetrio
+    const tetrio = this.document.getElementById('tetrio');
+
+    //hover
+    tetrio.addEventListener('mouseover', function(){
+        if (!isInside) {
+            isInside = true;
+            screenDiv.style.backgroundImage = "url('./Images/Tetris Background.png')";
+        }
+    });
+    tetrio.addEventListener('mouseout', () => {
+        screenDiv.innerHTML = ''
+        screenDiv.style.backgroundImage = 'none'
+        isInside = false;
+      });
+
+
+
+    //Chess
+    const chess = this.document.getElementById('chess');
+
+    //hover
+    chess.addEventListener('mouseover', function(){
+        if (!isInside) {
+            isInside = true;
+            screenDiv.style.backgroundImage = "url('./Images/Chess Background.png')";
+        }
+    });
+    chess.addEventListener('mouseout', () => {
+        screenDiv.innerHTML = ''
+        screenDiv.style.backgroundImage = 'none'
+        isInside = false;
+      });
+};
+
+
